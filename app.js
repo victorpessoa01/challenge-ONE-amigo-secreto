3f0a36e546e1f05863125e9b8f37c9d035231963
victorpessoa01 <victorpessoa0011@gmail.com>
2025-08-14 21:21:15 -0300

    Array de amigos adicionado

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+
+let friendsList = [];
+let friend = "";
+
+function addFriend() { 
+    friend = document.getElementById("amigo").value.trim();
+
+    if(friend !== "") {
+        if(friendsList.includes(friend)) {
+            alert("Este amigo já foi adicionado.");
+        } else {
+            friendsList.push(friend);
+        }
+    } else {
+        alert("Por favor, insira um nome válido.");
+    } 
+    
+    console.log(friendsList);
+    friend = "";
+    
+};
+
+console.log(friend);
+
